f7d3cbde56ba4cccc41058e64a84804262a14004
AMutaa <piusandru@gmail.com>
2019-04-19 10:21:25 -0400

    test- 2

--- __tests__/components/Search.test.js
+++ __tests__/components/Search.test.js
@@ -2,12 +2,25 @@ import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Search from '../../src/components/Search';
 
-
+// component renders
 describe('Search component', () => {
   test('renders', () => {
     const wrapper = shallow(<Search />);
     expect(wrapper.exists()).toBe(true)
   })
 })
+
+
+//testing input text
+test('user text is echoed ', () => {
+  const wrapper = shallow(<Search performSearch={() => { }} />)
+  wrapper.find('input').simulate('change', {
+    target: { value: 'hello' }
+  })
+
+  // assertion
+  expect(wrapper.find('input').props().value).toEqual('hello')
+})
+
 
 
