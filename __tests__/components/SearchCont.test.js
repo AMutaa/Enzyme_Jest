0c274cb7f8071c4c3dcec098d8bf3a82d57dad76
AMutaa <piusandru@gmail.com>
2019-04-19 11:48:12 -0400

    update

--- __tests__/components/SearchCont.test.js
+++ __tests__/components/SearchCont.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import SearchCont from '../../src/components/SearchCont';
 import Search from '../../src/components/Search';
+import fetchArticles from '../../src/api/__mocks__';
 
 
-jest.mock('../../src/api/api')
+jest.mock('../../src/api/api/')
 
 //component renders
 describe('Search container', () => {
@@ -12,10 +13,24 @@ describe('Search container', () => {
     const wrapper = shallow(<SearchCont />)
     expect(wrapper.exists()).toBe(true)
   })
-})
 
-test('should render Search component', () => {
-  const wrapper = mount(<SearchCont />);
+  // check if SearchCont has Search component as a child
+  test('should render Search component', () => {
+    const wrapper = mount(<SearchCont />);
+    expect(wrapper.children(Search).length).toEqual(1)
+  })
 
-  expect(wrapper.children(Search).length).toEqual(1)
+
+
+  // using our mock here
+  test('should update articles state', () => {
+    const wrapper = mount(<SearchCont />)
+    expect(wrapper.state().articles).toEqual([]);
+
+    const { performSearch } = wrapper.find(Search).props();
+
+    return performSearch().then(() => {
+      expect(wrapper.state().articles).toHaveLength(10);
+    })
+  })
 })
